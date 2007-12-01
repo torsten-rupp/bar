a8144d5d66f66540bc71d07aa4f5c8dbe87df5fe
Torsten Rupp <torsten.rupp@gmx.net>
2007-12-01 01:05:47 +0000

    thread functions

---
+++ bar/threads.c
@@ -0,0 +1,87 @@
+/***********************************************************************\
+*
+* $Source: /home/torsten/cvs/bar/threads.c,v $
+* $Revision: 1.1 $
+* $Author: torsten $
+* Contents: thread functions
+* Systems: all
+*
+\***********************************************************************/
+
+/****************************** Includes *******************************/
+#include <stdlib.h>
+#include <stdio.h>
+#include <pthread.h>
+#include <assert.h>
+
+#include "global.h"
+
+#include "threads.h"
+
+/****************** Conditional compilation switches *******************/
+
+/***************************** Constants *******************************/
+
+/***************************** Datatypes *******************************/
+
+/***************************** Variables *******************************/
+
+/****************************** Macros *********************************/
+
+/***************************** Forwards ********************************/
+
+/***************************** Functions *******************************/
+
+#ifdef __cplusplus
+  extern "C" {
+#endif
+
+bool Thread_init(Thread     *thread,
+                 int        priority,
+                 const void *entryFunction,
+                 void       *userData
+                )
+{
+  struct sched_param param;
+
+  assert(thread != NULL);
+
+  /* start thread */
+  if (pthread_create(&thread->handle,NULL,(void*(*)(void*))entryFunction,userData) != 0)
+  {
+    return FALSE;
+  }
+
+  /* set priority */
+  if (priority != 0)
+  {
+    param.sched_priority = priority;
+    if (pthread_setschedparam(thread->handle,
+                              SCHED_OTHER,
+                              &param
+                             ) != 0)
+    {
+      return FALSE;
+    }
+  }
+
+  return TRUE;
+}
+
+void Thread_done(Thread *thread)
+{
+  assert(thread != NULL);
+}
+
+void Thread_join(Thread *thread)
+{
+  assert(thread != NULL);
+
+  pthread_join(thread->handle,NULL);
+}
+
+#ifdef __cplusplus
+  }
+#endif
+
+/* end of file */
---
+++ bar/threads.h
@@ -0,0 +1,91 @@
+/***********************************************************************\
+*
+* $Source: /home/torsten/cvs/bar/threads.h,v $
+* $Revision: 1.1 $
+* $Author: torsten $
+* Contents: thread functions
+* Systems: all
+*
+\***********************************************************************/
+
+#ifndef __THREADS__
+#define __TRHEADS__
+
+/****************************** Includes *******************************/
+#include <stdlib.h>
+#include <stdio.h>
+#include <pthread.h>
+#include <assert.h>
+
+#include "global.h"
+
+/****************** Conditional compilation switches *******************/
+
+/***************************** Constants *******************************/
+
+/***************************** Datatypes *******************************/
+
+typedef struct
+{
+  pthread_t handle;
+} Thread;
+
+/***************************** Variables *******************************/
+
+/****************************** Macros *********************************/
+
+/***************************** Forwards ********************************/
+
+/***************************** Functions *******************************/
+
+#ifdef __cplusplus
+  extern "C" {
+#endif
+
+/***********************************************************************\
+* Name   : Thread_init
+* Purpose: init thread
+* Input  : thread        - thread variable
+*          priority      - priority or 0 for default priority
+*          entryFunction - thread entry function
+*          userData      - thread user data
+* Output : -
+* Return : TRUE if thread started, FALSE otherwise
+* Notes  : -
+\***********************************************************************/
+
+bool Thread_init(Thread     *thread,
+                 int        priority,
+                 const void *entryFunction,
+                 void       *userData
+                );
+
+/***********************************************************************\
+* Name   : Thread_done
+* Purpose: deinitialize thread
+* Input  : thread - thread
+* Output : -
+* Return : -
+* Notes  : -
+\***********************************************************************/
+
+void Thread_done(Thread *thread);
+
+/***********************************************************************\
+* Name   : Thread_join
+* Purpose: wait for termination of thread
+* Input  : thread - thread
+* Output : -
+* Return : -
+* Notes  : -
+\***********************************************************************/
+
+void Thread_join(Thread *thread);
+
+#ifdef __cplusplus
+  }
+#endif
+
+#endif /* __TRHEADS__ */
+
+/* end of file */
