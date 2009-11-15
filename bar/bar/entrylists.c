9e6c1acc17086a5bced4dd6de227daf1664ef014
Torsten Rupp <torsten.rupp@gmx.net>
2009-11-15 04:45:29 +0000

    entry list functions

---
+++ bar/bar/entrylists.c
@@ -0,0 +1,239 @@
+/***********************************************************************\
+*
+* $Source: /home/torsten/cvs/bar/bar/entrylists.c,v $
+* $Revision: 1.1 $
+* $Author: torsten $
+* Contents: Backup ARchiver entry list functions
+* Systems: all
+*
+\***********************************************************************/
+
+/****************************** Includes *******************************/
+#include "config.h"
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/types.h>
+#include <regex.h>
+#include <assert.h>
+
+#include "global.h"
+#include "lists.h"
+
+#include "bar.h"
+#include "patterns.h"
+
+#include "entrylists.h"
+
+/****************** Conditional compilation switches *******************/
+
+/***************************** Constants *******************************/
+
+/***************************** Datatypes *******************************/
+
+/***************************** Variables *******************************/
+
+/****************************** Macros *********************************/
+
+/***************************** Forwards ********************************/
+
+/***************************** Functions *******************************/
+
+#ifdef __cplusplus
+  extern "C" {
+#endif
+
+/***********************************************************************\
+* Name   : copyEntryNode
+* Purpose: copy allocated entry node
+* Input  : entryNode - entry node
+* Output : -
+* Return : copied entry node
+* Notes  : -
+\***********************************************************************/
+
+LOCAL EntryNode *copyEntryNode(EntryNode *entryNode,
+                               void      *userData
+                              )
+{
+  EntryNode *newEntryNode;
+  Errors      error;
+
+  assert(entryNode != NULL);
+
+  UNUSED_VARIABLE(userData);
+
+  /* allocate entry node */
+  newEntryNode = LIST_NEW_NODE(EntryNode);
+  if (newEntryNode == NULL)
+  {
+    HALT_INSUFFICIENT_MEMORY();
+  }
+
+  /* create entry */
+  newEntryNode->type   = entryNode->type;
+  newEntryNode->string = String_duplicate(entryNode->string);
+  error = Pattern_init(&newEntryNode->pattern,
+                       entryNode->string,
+                       entryNode->pattern.type
+                      );
+  if (error != ERROR_NONE)
+  {
+    String_delete(newEntryNode->string);
+    LIST_DELETE_NODE(newEntryNode);
+    return NULL;
+  }
+
+  return newEntryNode;
+}
+
+/***********************************************************************\
+* Name   : freeEntryNode
+* Purpose: free allocated entry node
+* Input  : entryNode - entry node
+* Output : -
+* Return : -
+* Notes  : -
+\***********************************************************************/
+
+LOCAL void freeEntryNode(EntryNode *entryNode,
+                         void      *userData
+                        )
+{
+  assert(entryNode != NULL);
+  assert(entryNode->string != NULL);
+
+  UNUSED_VARIABLE(userData);
+
+  Pattern_done(&entryNode->pattern);
+  String_delete(entryNode->string);
+  LIST_DELETE_NODE(entryNode);
+}
+
+/*---------------------------------------------------------------------*/
+
+Errors EntryList_initAll(void)
+{
+  return ERROR_NONE;
+}
+
+void EntryList_doneAll(void)
+{
+}
+
+void EntryList_init(EntryList *entryList)
+{
+  assert(entryList != NULL);
+
+  List_init(entryList);
+}
+
+void EntryList_done(EntryList *entryList)
+{
+  assert(entryList != NULL);
+
+  List_done(entryList,(ListNodeFreeFunction)freeEntryNode,NULL);
+}
+
+void EntryList_clear(EntryList *entryList)
+{
+  assert(entryList != NULL);
+
+  List_clear(entryList,(ListNodeFreeFunction)freeEntryNode,NULL);
+}
+
+void EntryList_copy(const EntryList *fromEntryList, EntryList *toEntryList)
+{
+  assert(fromEntryList != NULL);
+  assert(toEntryList != NULL);
+
+  List_copy(fromEntryList,toEntryList,NULL,NULL,NULL,(ListNodeCopyFunction)copyEntryNode,NULL);
+}
+
+void EntryList_move(EntryList *fromEntryList, EntryList *toEntryList)
+{
+  assert(fromEntryList != NULL);
+  assert(toEntryList != NULL);
+
+  List_move(fromEntryList,toEntryList,NULL,NULL,NULL);
+}
+
+Errors EntryList_append(EntryList    *entryList,
+                        EntryTypes   type,
+                        const String pattern,
+                        PatternTypes patternType
+                       )
+{
+  assert(entryList != NULL);
+  assert(pattern != NULL);
+
+  return EntryList_appendCString(entryList,type,String_cString(pattern),patternType);
+}
+
+Errors EntryList_appendCString(EntryList    *entryList,
+                               EntryTypes   type,
+                               const char   *pattern,
+                               PatternTypes patternType
+                              )
+{
+  EntryNode *entryNode;
+  Errors      error;
+
+  assert(entryList != NULL);
+  assert(pattern != NULL);
+
+  /* allocate entry node */
+  entryNode = LIST_NEW_NODE(EntryNode);
+  if (entryNode == NULL)
+  {
+    HALT_INSUFFICIENT_MEMORY();
+  }
+  entryNode->type   = type;
+  entryNode->string = String_newCString(pattern);
+
+  /* init pattern */
+  error = Pattern_initCString(&entryNode->pattern,
+                              pattern,
+                              patternType
+                             );
+  if (error != ERROR_NONE)
+  {
+    String_delete(entryNode->string);
+    LIST_DELETE_NODE(entryNode);
+    return error;
+  }
+
+  /* add to list */
+  List_append(entryList,entryNode);
+
+  return ERROR_NONE;
+}
+
+bool EntryList_match(const EntryList   *entryList,
+                     const String      string,
+                     PatternMatchModes patternMatchMode
+                    )
+{
+  bool      matchFlag;
+  EntryNode *entryNode;
+
+  assert(entryList != NULL);
+  assert(string != NULL);
+
+  matchFlag = FALSE;
+  entryNode = entryList->head;
+  while ((entryNode != NULL) && !matchFlag)
+  {
+    matchFlag = Pattern_match(&entryNode->pattern,string,patternMatchMode);
+    entryNode = entryNode->next;
+  }
+
+  return matchFlag;
+}
+
+#ifdef __cplusplus
+  }
+#endif
+
+/* end of file */
---
+++ bar/bar/entrylists.h
@@ -0,0 +1,192 @@
+/***********************************************************************\
+*
+* $Source: /home/torsten/cvs/bar/bar/entrylists.h,v $
+* $Revision: 1.1 $
+* $Author: torsten $
+* Contents: Backup ARchiver entry list functions
+* Systems: all
+*
+\***********************************************************************/
+
+#ifndef __ENTRYLISTS__
+#define __ENTRYLISTS__
+
+/****************************** Includes *******************************/
+#include "config.h"
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <regex.h>
+#include <assert.h>
+
+#include "global.h"
+#include "lists.h"
+#include "strings.h"
+#include "patterns.h"
+
+#include "errors.h"
+
+/****************** Conditional compilation switches *******************/
+
+/***************************** Constants *******************************/
+
+/***************************** Datatypes *******************************/
+
+typedef enum
+{
+  ENTRY_TYPE_FILE,
+  ENTRY_TYPE_IMAGE,
+} EntryTypes;
+
+typedef struct EntryNode
+{
+  LIST_NODE_HEADER(struct EntryNode);
+
+  EntryTypes type;
+  String     string;
+  Pattern    pattern;
+} EntryNode;
+
+typedef struct
+{
+  LIST_HEADER(EntryNode);
+} EntryList;
+
+/***************************** Variables *******************************/
+
+/****************************** Macros *********************************/
+
+/***************************** Forwards ********************************/
+
+/***************************** Functions *******************************/
+
+#ifdef __cplusplus
+  extern "C" {
+#endif
+
+/***********************************************************************\
+* Name   : EntryList_initAll
+* Purpose: init entry lists
+* Input  : -
+* Output : -
+* Return : ERROR_NONE or errorcode
+* Notes  : -
+\***********************************************************************/
+
+Errors EntryList_initAll(void);
+
+/***********************************************************************\
+* Name   : EntryList_doneAll
+* Purpose: deinitialize entry lists
+* Input  : -
+* Output : -
+* Return : ERROR_NONE or errorcode
+* Notes  : -
+\***********************************************************************/
+
+void EntryList_doneAll(void);
+
+/***********************************************************************\
+* Name   : EntryList_init
+* Purpose: init entry list
+* Input  : entryList - entry list
+* Output : -
+* Return : -
+* Notes  : -
+\***********************************************************************/
+
+void EntryList_init(EntryList *entryList);
+
+/***********************************************************************\
+* Name   : EntryList_done
+* Purpose: done entry list
+* Input  : entryList - entry list
+* Output : -
+* Return : -
+* Notes  : -
+\***********************************************************************/
+
+void EntryList_done(EntryList *entryList);
+
+/***********************************************************************\
+* Name   : EntryList_clear
+* Purpose: remove all entrys in list
+* Input  : entryList - entry list
+* Output : -
+* Return : -
+* Notes  : -
+\***********************************************************************/
+
+void EntryList_clear(EntryList *entryList);
+
+/***********************************************************************\
+* Name   : Entry_copyList
+* Purpose: copy all entrys from source list to destination list
+* Input  : fromEntryList - from entry list (source)
+*          toEntryList   - to entry list (destination)
+* Output : -
+* Return : -
+* Notes  : -
+\***********************************************************************/
+
+void EntryList_copy(const EntryList *fromEntryList, EntryList *toEntryList);
+
+/***********************************************************************\
+* Name   : EntryList_move
+* Purpose: move all entrys from source list to destination list
+* Input  : fromEntryList - from entry list (source)
+*          toEntryList   - to entry list (destination)
+* Output : -
+* Return : -
+* Notes  : -
+\***********************************************************************/
+
+void EntryList_move(EntryList *fromEntryList, EntryList *toEntryList);
+
+/***********************************************************************\
+* Name   : EntryList_append, EntryList_appendCString
+* Purpose: add entry to entry list
+* Input  : entryList   - entry list
++          type        - entry type; see ENTRY_TYPE_*
+*          pattern     - pattern
+*          patternType - pattern type; see PATTERN_TYPE_*
+* Output : -
+* Return : ERROR_NONE or error code
+* Notes  : -
+\***********************************************************************/
+
+Errors EntryList_append(EntryList    *entryList,
+                        EntryTypes   type,
+                        const String pattern,
+                        PatternTypes patternType
+                       );
+Errors EntryList_appendCString(EntryList    *entryList,
+                               EntryTypes   type,
+                               const char   *pattern,
+                               PatternTypes patternType
+                              );
+
+/***********************************************************************\
+* Name   : EntryList_match
+* Purpose: patch string with all entrys of list
+* Input  : entryList        - entry list
+*          s                - string
+*          patternMatchMode - pattern match mode; see PatternMatchModes
+* Output : -
+* Return : TRUE if entry match, FALSE otherwise
+* Notes  : -
+\***********************************************************************/
+
+bool EntryList_match(const EntryList   *entryList,
+                     const String      string,
+                     PatternMatchModes patternMatchMode
+                    );
+
+#ifdef __cplusplus
+  }
+#endif
+
+#endif /* __PATTERNLISTS__ */
+
+/* end of file */
