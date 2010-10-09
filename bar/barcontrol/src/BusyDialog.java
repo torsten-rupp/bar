6c4c822224ac67ed3e7e3acd6f5a90a591df91ed
Torsten Rupp <torsten.rupp@gmx.net>
2010-10-09 13:09:45 +0000

    support two progress bars

--- bar/barcontrol/src/BusyDialog.java
+++ bar/barcontrol/src/BusyDialog.java
@@ -1,7 +1,7 @@
 /***********************************************************************\
 *
 * $Source: /home/torsten/cvs/bar/barcontrol/src/BusyDialog.java,v $
-* $Revision: 1.5 $
+* $Revision: 1.6 $
 * $Author: torsten $
 * Contents: busy dialog
 * Systems: all
@@ -35,22 +35,28 @@ import org.eclipse.swt.widgets.Text;
 class BusyDialog
 {
   // --------------------------- constants --------------------------------
+  public final static int NONE          = 0;
+  public final static int TEXT0         = 1 << 0;
+  public final static int TEXT1         = 1 << 1;
+  public final static int PROGRESS_BAR0 = 1 << 2;
+  public final static int PROGRESS_BAR1 = 1 << 3;
 
   // --------------------------- variables --------------------------------
-  private Display display;
+  private Display     display;
 
-  private Image   images[] = new Image[2];
-  private long    imageTimestamp;
-  private int     imageIndex;
+  private Image       images[] = new Image[2];
+  private long        imageTimestamp;
+  private int         imageIndex;
 
-  private Shell   dialog;
-  private Label   widgetImage;
-  private Label   widgetMessage;
-  private Label   widgetText;
-  private Button  widgetAbortButton;
+  private Shell       dialog;
+  private Label       widgetImage;
+  private Label       widgetMessage;
+  private Label       widgetText0,widgetText1;
+  private ProgressBar widgetProgressBar0,widgetProgressBar1;
+  private Button      widgetAbortButton;
 
-  private boolean abortedFlag;
-  private boolean resizedFlag;
+  private boolean     abortedFlag;
+  private boolean     resizedFlag;
 
   // ------------------------ native functions ----------------------------
 
@@ -62,7 +68,7 @@ class BusyDialog
    * @param width,height width/height of dialog
    * @param message dialog message
    */
-  public BusyDialog(Shell parentShell, String title, int width, int height, String message)
+  public BusyDialog(Shell parentShell, String title, int width, int height, String message, int flags)
   {
     TableLayout     tableLayout;
     TableLayoutData tableLayoutData;
@@ -100,21 +106,61 @@ class BusyDialog
       subComposite.setLayout(new TableLayout(new double[]{0.0,1.0},1.0));
       subComposite.setLayoutData(new TableLayoutData(0,1,TableLayoutData.NSWE));
       {
+        int row = 0;
+
         if (message != null)
         {
           widgetMessage = new Label(subComposite,SWT.LEFT);
           widgetMessage.setText(message);
-          widgetMessage.setLayoutData(new TableLayoutData(0,0,TableLayoutData.N|TableLayoutData.WE));
-
-          widgetText = new Label(subComposite,SWT.LEFT);
-          widgetText.setLayoutData(new TableLayoutData(1,0,TableLayoutData.S|TableLayoutData.WE));
+          widgetMessage.setLayoutData(new TableLayoutData(row,0,TableLayoutData.N|TableLayoutData.WE)); row++;
         }
         else
         {
           widgetMessage = null;
+        }
 
-          widgetText = new Label(subComposite,SWT.LEFT);
-          widgetText.setLayoutData(new TableLayoutData(0,0,TableLayoutData.WE));
+        if ((flags & TEXT0) != 0)
+        {
+          widgetText0 = new Label(subComposite,SWT.LEFT);
+          widgetText0.setLayoutData(new TableLayoutData(row,0,TableLayoutData.WE)); row++;
+        }
+        else
+        {
+          widgetText0 = null;
+        }
+
+        if ((flags & PROGRESS_BAR0) != 0)
+        {
+          widgetProgressBar0 = new ProgressBar(subComposite,SWT.LEFT);
+          widgetProgressBar0.setLayoutData(new TableLayoutData(row,0,TableLayoutData.WE)); row++;
+          widgetProgressBar0.setMinimum(0.0);
+          widgetProgressBar0.setMaximum(100.0);
+        }
+        else
+        {
+          widgetProgressBar0 = null;
+        }
+
+        if ((flags & TEXT1) != 0)
+        {
+          widgetText1 = new Label(subComposite,SWT.LEFT);
+          widgetText1.setLayoutData(new TableLayoutData(row,0,TableLayoutData.WE)); row++;
+        }
+        else
+        {
+          widgetText1 = null;
+        }
+
+        if ((flags & PROGRESS_BAR1) != 0)
+        {
+          widgetProgressBar1 = new ProgressBar(subComposite,SWT.LEFT);
+          widgetProgressBar1.setLayoutData(new TableLayoutData(row,0,TableLayoutData.WE)); row++;
+          widgetProgressBar1.setMinimum(0.0);
+          widgetProgressBar1.setMaximum(100.0);
+        }
+        else
+        {
+          widgetProgressBar1 = null;
         }
       }
     }
@@ -191,6 +237,16 @@ class BusyDialog
     resizedFlag = false;
     abortedFlag = false;
     dialog.open();
+  }
+
+  /** create busy dialog
+   * @param parentShell parent shell
+   * @param title dialog title
+   * @param width,height width/height of dialog
+   */
+  public BusyDialog(Shell parentShell, String title, int width, int height, String message)
+  {
+    this(parentShell,title,width,height,message,NONE);
   }
 
   /** create busy dialog
@@ -279,11 +335,12 @@ class BusyDialog
     }
   }
 
-  /** update busy dialog
+  /** update busy dialog text
+   * @param i index 0|1
    * @param text text to show (can be null)
    * @return true if closed, false otherwise
    */
-  public boolean update(final String text)
+  public boolean updateText(final int i, final String text)
   {
     if (!dialog.isDisposed())
     {
@@ -301,7 +358,13 @@ class BusyDialog
 
           if (text != null)
           {
-            widgetText.setText(text);
+            Label widgetText        = null;
+            switch (i)
+            {
+              case 0: widgetText = widgetText0; break;
+              case 1: widgetText = widgetText1; break;
+            }
+            if ((text != null) && (widgetText != null)) widgetText.setText(text);
 
             /* resize dialog (it not manually changed) */
             if (!resizedFlag)
@@ -326,19 +389,99 @@ class BusyDialog
     }
   }
 
-  /** update busy dialog
-   * @param n number to show
+  /** update busy dialog progress bar
+   * @param i index 0|1
+   * @param n progress value
+   * @return true if closed, false otherwise
    */
-  public boolean update(Long n)
+  public boolean updateProgressBar(final int i, final double n)
   {
-    return update(Long.toString(n));
+    if (!dialog.isDisposed())
+    {
+      display.syncExec(new Runnable()
+      {
+        public void run()
+        {
+          long timestamp = System.currentTimeMillis();
+          if (timestamp > (imageTimestamp+250))
+          {
+            imageTimestamp = timestamp;
+            imageIndex     = (imageIndex+1)%2;
+            widgetImage.setImage(images[imageIndex]);
+          }
+
+          ProgressBar widgetProgressBar = null;
+          switch (i)
+          {
+            case 0: widgetProgressBar = widgetProgressBar0; break;
+            case 1: widgetProgressBar = widgetProgressBar1; break;
+          }
+          if (widgetProgressBar != null) widgetProgressBar.setSelection(n);
+
+          display.update();
+        }
+      });
+
+      return true;
+    }
+    else
+    {
+      return false;
+    }
   }
 
   /** update busy dialog
+   * @param i index 0|1
+   * @param n number to show
+   * @return true if closed, false otherwise
+   */
+  public boolean updateText(int i, Long n)
+  {
+    return updateText(i,Long.toString(n));
+  }
+
+  /** update busy dialog
+   * @param i index 0|1
+   * @return true if closed, false otherwise
+   */
+  public boolean update(int i)
+  {
+    return updateText(i,(String)null);
+  }
+
+  /** update busy dialog text
+   * @param text text to show (can be null)
+   * @return true if closed, false otherwise
+   */
+  public boolean updateText(String text)
+  {
+    return updateText(0,text);
+  }
+
+  /** update busy dialog progress bar
+   * @param n progress value
+   * @return true if closed, false otherwise
+   */
+  public boolean updateProgressBar(double n)
+  {
+    return updateProgressBar(0,n);
+  }
+
+  /** update busy dialog
+   * @param n number to show
+   * @return true if closed, false otherwise
+   */
+  public boolean updateText(Long n)
+  {
+    return updateText(0,Long.toString(n));
+  }
+
+  /** update busy dialog
+   * @return true if closed, false otherwise
    */
   public boolean update()
   {
-    return update((String)null);
+    return update(0);
   }
 }
 
