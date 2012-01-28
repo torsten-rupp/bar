05f2ac27b7657d3aeed02881f8af7057a890af23
Torsten Rupp <torsten.rupp@gmx.net>
2012-01-28 10:48:13 +0000

    fixed header information

--- bar/bar/commands_compare.c
+++ bar/bar/commands_compare.c
@@ -1,8 +1,8 @@
 /***********************************************************************\
 *
-* $Source: /home/torsten/cvs/bar/bar/commands_compare.c,v $
-* $Revision: 1.12 $
-* $Author: torsten $
+* $Revision$
+* $Date$
+* $Author$
 * Contents: Backup ARchiver archive compare function
 * Systems : all
 *
