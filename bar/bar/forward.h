f03fbc5293c43bf8fd8ae2f34e42acd50284f8ab
Torsten Rupp <torsten.rupp@gmx.net>
2012-04-08 14:13:03 +0000

    forward declarations

---
+++ bar/bar/forward.h
@@ -0,0 +1,44 @@
+/***********************************************************************\
+*
+* $Revision$
+* $Date$
+* $Author$
+* Contents: Backup ARchiver forward declarations
+* Systems: all
+*
+\***********************************************************************/
+
+#ifndef __FORWARD__
+#define __FORWARD__
+
+/****************************** Includes *******************************/
+#include "config.h"
+
+/****************** Conditional compilation switches *******************/
+
+/***************************** Constants *******************************/
+
+/***************************** Datatypes *******************************/
+
+// job options type forward declaration
+typedef struct JobOptions JobOptions;
+
+/***************************** Variables *******************************/
+
+/****************************** Macros *********************************/
+
+/***************************** Forwards ********************************/
+
+/***************************** Functions *******************************/
+
+#ifdef __cplusplus
+  extern "C" {
+#endif
+
+#ifdef __cplusplus
+  }
+#endif
+
+#endif /* __BAR__ */
+
+/* end of file */
