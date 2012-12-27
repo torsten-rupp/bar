8b1f6817bc1532cd24b9f777faf10c9520e46624
Torsten Rupp <torsten.rupp@gmx.net>
2012-12-27 08:37:49 +0000

    - support cross-compilation Linux -> Windows with MinGW compiler - support usage of PCRE instead standard regex (required on Windows; can be used on Linux, too)

--- bar/bar/patternlists.h
+++ bar/bar/patternlists.h
@@ -17,7 +17,13 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <sys/types.h>
-#include <regex.h>
+#if defined(HAVE_PCRE)
+  #include <pcreposix.h>
+#elif defined(HAVE_REGEX_H)
+  #include <regex.h>
+#else
+  #error No regular expression library available!
+#endif /* HAVE_PCRE || HAVE_REGEX_H */
 #include <assert.h>
 
 #include "global.h"
