28342cf9277db6975f9167584a2e4307b6511f18
Torsten Rupp <torsten.rupp@gmx.net>
2013-02-03 15:42:14 +0000

    fix config.h include: use <> to support separated build directory

--- bar/bar/storage.c
+++ bar/bar/storage.c
@@ -9,7 +9,7 @@
 \***********************************************************************/
 
 /****************************** Includes *******************************/
-#include "config.h"
+#include <config.h>  // use <...> to support separated build directory
 
 #include <stdlib.h>
 #include <stdio.h>
