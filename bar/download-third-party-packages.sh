2ef262db4b457c79350b751011e797b01ecf1a73
Torsten Rupp <torsten.rupp@gmx.net>
2014-02-16 10:37:13 +0000

    upgraded libcdio to 0.92

--- bar/download-third-party-packages.sh
+++ bar/download-third-party-packages.sh
@@ -634,7 +634,7 @@ if test $cleanFlag -eq 0; then
   fi
 
   if test $allFlag -eq 1 -o $libcdioFlag -eq 1; then
-    # libcdio 0.82
+    # libcdio 0.92
     (
      if test -n "$destination"; then
        cd $destination
@@ -642,15 +642,15 @@ if test $cleanFlag -eq 0; then
        cd $tmpDirectory
      fi
 
-     if test ! -f libcdio-0.82.tar.gz; then
-       $WGET $WGET_OPTIONS 'ftp://ftp.gnu.org/gnu/libcdio/libcdio-0.82.tar.gz'
+     if test ! -f libcdio-0.92.tar.gz; then
+       $WGET $WGET_OPTIONS 'ftp://ftp.gnu.org/gnu/libcdio/libcdio-0.92.tar.gz'
      fi
      if test $noDecompressFlag -eq 0; then
-       $TAR xzf libcdio-0.82.tar.gz
+       $TAR xzf libcdio-0.92.tar.gz
      fi
     )
     if test $noDecompressFlag -eq 0; then
-      $LN -f -s $tmpDirectory/libcdio-0.82 libcdio
+      $LN -f -s $tmpDirectory/libcdio-0.92 libcdio
     fi
   fi
 
