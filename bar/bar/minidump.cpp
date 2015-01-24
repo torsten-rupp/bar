e3f92c9b092525fc496829dd72b3b1df3d73044e
Torsten Rupp <torsten.rupp@gmx.net>
2015-01-24 10:42:09 +0000

    fixed create minidump

--- bar/bar/minidump.cpp
+++ bar/bar/minidump.cpp
@@ -14,6 +14,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <sys/utsname.h>
 #include <assert.h>
 
 #ifdef HAVE_BREAKPAD
@@ -38,9 +39,9 @@
 
 /***************************** Variables *******************************/
 #ifdef HAVE_BREAKPAD
-  LOCAL bool                                initFlag = FALSE;
-  LOCAL char                                minidumpFileName[1024];
-  LOCAL int                                 minidumpFileDescriptor;
+  LOCAL bool               initFlag = FALSE;
+  LOCAL char               minidumpFileName[1024];
+  LOCAL int                minidumpFileDescriptor;
   LOCAL google_breakpad::MinidumpDescriptor *minidumpDescriptor;
   LOCAL google_breakpad::ExceptionHandler   *exceptionHandler;
 #endif /* HAVE_BREAKPAD */
@@ -52,26 +53,71 @@
 /***************************** Functions *******************************/
 
 #ifdef HAVE_BREAKPAD
+/***********************************************************************\
+* Name   : printString
+* Purpose: print string
+* Input  : s - string to print
+* Output : -
+* Return : -
+* Notes  : Do not use printf
+\***********************************************************************/
+
+LOCAL void printString(const char *s)
+{
+  (void)write(STDERR_FILENO,s,strlen(s));
+}
+
+/***********************************************************************\
+* Name   : minidumpCallback
+* Purpose: minidump call back
+* Input  : minidumpDescriptor - minidump descriptor
+*          context            - context
+*          succeeded          - succeeded
+* Output : -
+* Return : -
+* Notes  : -
+\***********************************************************************/
+
 LOCAL bool minidumpCallback(const google_breakpad::MinidumpDescriptor &minidumpDescriptor,
                             void                                      *context,
                             bool                                      succeeded
                            )
 {
-  #define ERROR_TEXT1 "+++ Created crash dump file '"
-  #define ERROR_TEXT2 "'. Please send the crash dump file to torsten.rupp@gmx.net +++\n."
+  struct utsname utsname;
+
+  #define __TO_STRING(z) __TO_STRING_TMP(z)
+  #define __TO_STRING_TMP(z) #z
+  #define VERSION_MAJOR_STRING __TO_STRING(VERSION_MAJOR)
+  #define VERSION_MINOR_STRING __TO_STRING(VERSION_MINOR)
+  #define VERSION_SVN_STRING __TO_STRING(VERSION_SVN)
+  #define VERSION_STRING VERSION_MAJOR_STRING "." VERSION_MINOR_STRING " (rev. " VERSION_SVN_STRING ")"
 
   close(minidumpFileDescriptor);
 
+  uname(&utsname);
+
   // Note: do not use fprintf; it does not work here
-  write(STDERR_FILENO,ERROR_TEXT1,strlen(ERROR_TEXT1));
-  write(STDERR_FILENO,minidumpFileName,strlen(minidumpFileName));
-  write(STDERR_FILENO,ERROR_TEXT2,strlen(ERROR_TEXT2));
+  printString("+++ BAR CRASH DUMP ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n");
+  printString("Dump file   : "); printString(minidumpFileName); printString("\n");
+  printString("Version     : "); printString(VERSION_STRING); printString("\n");
+  printString("OS          : "); printString(utsname.sysname); printString(", "); printString(utsname.release); printString(", "); printString(utsname.machine);  printString("\n");
+  printString("Please send the crash dump file and this information to torsten.rupp@gmx.net\n");
+  printString("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n");
 
   return succeeded;
+
+  #undef __TO_STRING
+  #undef __TO_STRING_TMP
+  #undef VERSION_MAJOR_STRING
+  #undef VERSION_MINOR_STRING
+  #undef VERSION_SVN_STRING
+  #undef VERSION_STRING
 }
 #endif /* HAVE_BREAKPAD */
 
 /*---------------------------------------------------------------------*/
+
+//LOCAL void crash() { volatile int* a = (int*)(NULL); *a = 1; }
 
 bool MiniDump_init(void)
 {
@@ -84,7 +130,7 @@ bool MiniDump_init(void)
              MINIDUMP_FILENAME
     );
 
-    // open minidump file
+    // open minidump file./src/google_breakpad/processor/minidump_processor.h
     minidumpFileDescriptor = open(minidumpFileName,O_CREAT|O_RDWR,0660);
     if (minidumpFileDescriptor == -1)
     {
@@ -107,7 +153,7 @@ bool MiniDump_init(void)
                                                              true,
                                                              -1
                                                             );
-    if (minidumpDescriptor == NULL)
+    if (exceptionHandler == NULL)
     {
       delete minidumpDescriptor;
       (void)close(minidumpFileDescriptor);
@@ -117,6 +163,8 @@ bool MiniDump_init(void)
 
     initFlag = TRUE;
   #endif /* HAVE_BREAKPAD */
+
+//crash();
 
   return TRUE;
 }
