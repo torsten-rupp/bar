3ddb0b5c95692e0cabc13b1819c90d5ba805ecf3
Torsten Rupp <torsten.rupp@gmx.net>
2015-09-19 08:58:57 +0000

    add changelog to *.spec files

--- bar/osc/changelog.pl
+++ bar/osc/changelog.pl
@@ -28,6 +28,21 @@ use Getopt::Long;
 
 # ------------------------------ main program  -------------------------------
 
+GetOptions ("type=s" => \$type,
+            "help"   => \$help
+           );
+if    ($type eq "debian")
+{
+}
+elsif ($type eq "spec")
+{
+}
+else
+{
+  print STDERR "Unknown type '$type'!\n";
+  exit 1;
+}
+
 my $line;
 my $lineNb=0;
 my $changeLogFlag=0;
@@ -42,23 +57,60 @@ while ($line=<STDIN>)
   {
     if    ($line =~ /^\s*$/)
     {
-      print "\n";
-      print " -- Torsten Rupp <torsten.rupp\@gmx.net>  $date\n";
+      if    ($type eq "debian")
+      {
+        print "\n";
+        print " -- Torsten Rupp <torsten.rupp\@gmx.net>  $date\n";
+        print "\n";
+      }
+      elsif ($type eq "spec")
+      {
+        print "\n";
+      }
+
       $changeLogFlag=0;
+    }
+    elsif ($line =~ /\s*\*\s*(\S.*)/)
+    {
+      if    ($type eq "debian")
+      {
+        print "  * $1\n";
+      }
+      elsif ($type eq "spec")
+      {
+        print "  - $1\n";
+      }
+    }
+    elsif ($line =~ /\s*(\S.*)/)
+    {
+      print "    $1\n";
     }
     else
     {
-      print "$line\n";
     }
   }
   else
   {
-    if    ($line =~ /^(\S+)\s+(.*)\s*$/)
+    if    ($line =~ /^(\S+)\s+(\S+).*\s*$/)
     {
-      print "bar ($2) stable; urgency=low\n";
-      open(HANDLE,"date -d $1 '+%a, %d %b %Y %H:%M:%S %z'|");
-      read(HANDLE,$date,1024);
-      close(HANDLE);
+      if    ($type eq "debian")
+      {
+        open(HANDLE,"date -d $1 '+%a, %d %b %Y %H:%M:%S %z'|");
+        read(HANDLE,$date,1024);
+        close(HANDLE);
+        chop $date;
+
+        print "bar ($2) stable; urgency=low\n";
+      }
+      elsif ($type eq "spec")
+      {
+        open(HANDLE,"date -d $1 '+%a %b %d %Y'|");
+        read(HANDLE,$date,1024);
+        close(HANDLE);
+        chop $date;
+
+        print "* $date Torsten Rupp <torsten.rupp\@gmx.net> $2\n";
+      }
       $changeLogFlag=1;
     }
     elsif (($line =~ /^\s*$/) || ($line =~ /^\s*\/\//))
