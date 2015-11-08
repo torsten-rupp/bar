7c8b795d5f71089755f659face35762b6aa8298e
Torsten Rupp <torsten.rupp@gmx.net>
2015-11-08 05:29:41 +0000

    added indizes

--- bar/bar/index_definition.sql
+++ bar/bar/index_definition.sql
@@ -3,7 +3,7 @@
  Note: SQLite3 require syntax "CREATE TABLE foo(" in a single line!
 
 */
-VERSION = 4;
+VERSION = 5;
 
 PRAGMA foreign_keys = ON;
 PRAGMA auto_vacuum = INCREMENTAL;
@@ -39,6 +39,7 @@ CREATE TABLE IF NOT EXISTS storage(
 
   FOREIGN KEY(entityId) REFERENCES entities(id)
 );
+CREATE INDEX IF NOT EXISTS storagesIndex ON storage (entityId,name);
 
 CREATE TABLE IF NOT EXISTS files(
   id              INTEGER PRIMARY KEY,
@@ -142,6 +143,138 @@ CREATE INDEX IF NOT EXISTS specialIndex ON special (storageId,name);
 /*
 
 // ---------------------------------------------------------------------
+
+VERSION = 4;
+
+PRAGMA foreign_keys = ON;
+PRAGMA auto_vacuum = INCREMENTAL;
+
+CREATE TABLE IF NOT EXISTS meta(
+  name  TEXT UNIQUE,
+  value TEXT
+);
+INSERT OR IGNORE INTO meta (name,value) VALUES ('version',$version);
+INSERT OR IGNORE INTO meta (name,value) VALUES ('datetime',DATETIME('now'));
+
+CREATE TABLE IF NOT EXISTS entities(
+  id              INTEGER PRIMARY KEY,
+  jobUUID         TEXT NOT NULL,
+  scheduleUUID    TEXT NOT NULL,
+  created         INTEGER,
+  type            INTEGER,
+  parentJobUUID   INTEGER,
+  bidFlag         INTEGER
+);
+
+CREATE TABLE IF NOT EXISTS storage(
+  id              INTEGER PRIMARY KEY,
+  entityId        INTEGER,
+  name            TEXT NOT NULL,
+  created         INTEGER,
+  entries         INTEGER,
+  size            INTEGER,
+  state           INTEGER,
+  mode            INTEGER,
+  lastChecked     INTEGER,
+  errorMessage    TEXT,
+
+  FOREIGN KEY(entityId) REFERENCES entities(id)
+);
+
+CREATE TABLE IF NOT EXISTS files(
+  id              INTEGER PRIMARY KEY,
+  storageId       INTEGER,
+  name            TEXT,
+  size            INTEGER,
+  timeLastAccess  INTEGER,
+  timeModified    INTEGER,
+  timeLastChanged INTEGER,
+  userId          INTEGER,
+  groupId         INTEGER,
+  permission      INTEGER,
+  fragmentOffset  INTEGER,
+  fragmentSize    INTEGER,
+
+  FOREIGN KEY(storageId) REFERENCES storage(id)
+);
+
+CREATE TABLE IF NOT EXISTS images(
+  id              INTEGER PRIMARY KEY,
+  storageId       INTEGER,
+  name            TEXT,
+  fileSystemType  INTEGER,
+  size            INTEGER,
+  blockSize       INTEGER,
+  blockOffset     INTEGER,
+  blockCount      INTEGER,
+
+  FOREIGN KEY(storageId) REFERENCES storage(id)
+);
+CREATE INDEX IF NOT EXISTS imagesIndex ON images (storageId,name);
+
+CREATE TABLE IF NOT EXISTS directories(
+  id              INTEGER PRIMARY KEY,
+  storageId       INTEGER,
+  name            TEXT,
+  timeLastAccess  INTEGER,
+  timeModified    INTEGER,
+  timeLastChanged INTEGER,
+  userId          INTEGER,
+  groupId         INTEGER,
+  permission      INTEGER,
+
+  FOREIGN KEY(storageId) REFERENCES storage(id)
+);
+
+CREATE TABLE IF NOT EXISTS links(
+  id              INTEGER PRIMARY KEY,
+  storageId       INTEGER,
+  name            TEXT,
+  destinationName TEXT,
+  timeLastAccess  INTEGER,
+  timeModified    INTEGER,
+  timeLastChanged INTEGER,
+  userId          INTEGER,
+  groupId         INTEGER,
+  permission      INTEGER,
+
+  FOREIGN KEY(storageId) REFERENCES storage(id)
+);
+
+CREATE TABLE IF NOT EXISTS hardlinks(
+  id              INTEGER PRIMARY KEY,
+  storageId       INTEGER,
+  name            TEXT,
+  size            INTEGER,
+  timeLastAccess  INTEGER,
+  timeModified    INTEGER,
+  timeLastChanged INTEGER,
+  userId          INTEGER,
+  groupId         INTEGER,
+  permission      INTEGER,
+  fragmentOffset  INTEGER,
+  fragmentSize    INTEGER,
+
+  FOREIGN KEY(storageId) REFERENCES storage(id)
+);
+
+CREATE TABLE IF NOT EXISTS special(
+  id              INTEGER PRIMARY KEY,
+  storageId       INTEGER,
+  name            TEXT,
+  specialType     INTEGER,
+  timeLastAccess  INTEGER,
+  timeModified    INTEGER,
+  timeLastChanged INTEGER,
+  userId          INTEGER,
+  groupId         INTEGER,
+  permission      INTEGER,
+  major           INTEGER,
+  minor           INTEGER,
+
+  FOREIGN KEY(storageId) REFERENCES storage(id)
+);
+
 
 VERSION = 3;
 
