f7505d21864c77324f6d80179989eef84bd2ea70
Torsten Rupp <torsten.rupp@gmx.net>
2016-02-14 19:58:56 +0000

    added index

--- bar/bar/index_definition.sql
+++ bar/bar/index_definition.sql
@@ -18,32 +18,42 @@ INSERT OR IGNORE INTO meta (name,value) VALUES ('datetime',DATETIME('now'));
 
 // --- entities --------------------------------------------------------
 CREATE TABLE IF NOT EXISTS entities(
-  id              INTEGER PRIMARY KEY,
-  jobUUID         TEXT NOT NULL,
-  scheduleUUID    TEXT NOT NULL,
-  created         INTEGER,
-  type            INTEGER,
-  parentJobUUID   INTEGER,
-  bidFlag         INTEGER
+  id                 INTEGER PRIMARY KEY,
+  jobUUID            TEXT NOT NULL,
+  scheduleUUID       TEXT NOT NULL,
+  created            INTEGER,
+  type               INTEGER,
+  parentJobUUID      INTEGER,
+  bidFlag            INTEGER,
+
+  totalStoragesCount INTEGER DEFAULT 0,
+  totalStoragesSize  INTEGER DEFAULT 0,
+  totalEntriesCount  INTEGER DEFAULT 0,
+  totalEntriesSize   INTEGER DEFAULT 0
 );
 INSERT OR IGNORE INTO entities (id,jobUUID,scheduleUUID,created,type,parentJobUUID,bidFlag) VALUES (0,'','',0,0,0,0);
+CREATE INDEX IF NOT EXISTS entitiesIndex ON entities (jobUUID);
 
 // --- storage ---------------------------------------------------------
 CREATE TABLE IF NOT EXISTS storage(
-  id              INTEGER PRIMARY KEY,
-  entityId        INTEGER,
-  name            TEXT NOT NULL,
-  created         INTEGER,
-  size            INTEGER,
-  state           INTEGER,
-  mode            INTEGER,
-  lastChecked     INTEGER,
-  errorMessage    TEXT
+  id                INTEGER PRIMARY KEY,
+  entityId          INTEGER,
+  name              TEXT NOT NULL,
+  created           INTEGER,
+  size              INTEGER,
+  state             INTEGER,
+  mode              INTEGER,
+  lastChecked       INTEGER,
+  errorMessage      TEXT,
+
+  totalEntriesCount INTEGER DEFAULT 0,
+  totalEntriesSize  INTEGER DEFAULT 0
 
   // Note: no foreign key entityId: storage may be created temporary before an entity
   // FOREIGN KEY(entityId) REFERENCES entities(id)
 );
 CREATE INDEX IF NOT EXISTS storagesIndex ON storage (entityId,name);
+
 CREATE VIRTUAL TABLE FTS_storage USING FTS4(
   storageId,
   name,
@@ -52,15 +62,43 @@ CREATE VIRTUAL TABLE FTS_storage USING FTS4(
 //  tokenize=unicode61 'tokenchars= !"#$%&''()*+,-:;<=>?@[\]^_`{|}~' 'separators=/.' 'remove_diacritics=0'
   tokenize=unicode61
 );
+
 CREATE TRIGGER storageInsert AFTER INSERT ON storage
   BEGIN
+    UPDATE entities
+      SET totalStoragesCount=totalStoragesCount+1,
+          totalStoragesSize =totalStoragesSize +NEW.totalEntriesSize,
+          totalEntriesCount =totalEntriesCount +NEW.totalEntriesCount,
+          totalEntriesSize  =totalEntriesSize  +NEW.totalEntriesSize
+      WHERE entities.id=NEW.entityId;
     INSERT INTO FTS_storage VALUES (NEW.id,NEW.name);
   END;
 CREATE TRIGGER storageDelete BEFORE DELETE ON storage
   BEGIN
+    UPDATE entities
+      SET totalStoragesCount=totalStoragesCount-1,
+          totalStoragesSize =totalStoragesSize -OLD.totalEntriesSize,
+          totalEntriesCount =totalEntriesCount -OLD.totalEntriesCount,
+          totalEntriesSize  =totalEntriesSize  -OLD.totalEntriesSize
+      WHERE entities.id=OLD.entityId;
     DELETE FROM FTS_storage WHERE storageId = OLD.id;
   END;
-CREATE TRIGGER storageUpdate AFTER UPDATE OF name ON storage
+CREATE TRIGGER storageUpdateSize AFTER UPDATE OF entityId,totalEntriesCount,totalEntriesSize ON storage
+  BEGIN
+    UPDATE entities
+      SET totalStoragesCount=totalStoragesCount-1,
+          totalStoragesSize =totalStoragesSize -OLD.totalEntriesSize,
+          totalEntriesCount =totalEntriesCount -OLD.totalEntriesCount,
+          totalEntriesSize  =totalEntriesSize  -OLD.totalEntriesSize
+      WHERE entities.id=OLD.entityId;
+    UPDATE entities
+      SET totalStoragesCount=totalStoragesCount+1,
+          totalStoragesSize =totalStoragesSize +NEW.totalEntriesSize,
+          totalEntriesCount =totalEntriesCount +NEW.totalEntriesCount,
+          totalEntriesSize  =totalEntriesSize  +NEW.totalEntriesSize
+      WHERE entities.id=NEW.entityId;
+  END;
+CREATE TRIGGER storageUpdateName AFTER UPDATE OF name ON storage
   BEGIN
     DELETE FROM FTS_storage WHERE storageId = OLD.id;
     INSERT INTO FTS_storage VALUES (NEW.id,NEW.name);
@@ -84,6 +122,7 @@ CREATE TABLE IF NOT EXISTS files(
   FOREIGN KEY(storageId) REFERENCES storage(id)
 );
 CREATE INDEX IF NOT EXISTS filesIndex ON files (storageId,name);
+
 CREATE VIRTUAL TABLE FTS_files USING FTS4(
   fileId,
   name,
@@ -92,15 +131,35 @@ CREATE VIRTUAL TABLE FTS_files USING FTS4(
 //  tokenize=unicode61 'tokenchars= !"#$%&''()*+,-:;<=>?@[\]^_`{|}~' 'separators=/.' 'remove_diacritics=0'
   tokenize=unicode61
 );
-CREATE TRIGGER fileInsert AFTER INSERT ON files
+
+CREATE TRIGGER filesInsert AFTER INSERT ON files
   BEGIN
+    UPDATE storage
+      SET totalEntriesCount=totalEntriesCount+1,
+          totalEntriesSize =totalEntriesSize +NEW.size
+      WHERE storage.id=NEW.storageId;
     INSERT INTO FTS_files VALUES (NEW.id,NEW.name);
   END;
-CREATE TRIGGER fileDelete BEFORE DELETE ON files
+CREATE TRIGGER filesDelete BEFORE DELETE ON files
   BEGIN
+    UPDATE storage
+      SET totalEntriesCount=totalEntriesCount-1,
+          totalEntriesSize =totalEntriesSize -OLD.size
+      WHERE storage.id=OLD.storageId;
     DELETE FROM FTS_files WHERE fileId = OLD.id;
   END;
-CREATE TRIGGER fileUpdate AFTER UPDATE OF name ON files
+CREATE TRIGGER filesUpdateSize AFTER UPDATE OF storageId,size ON files
+  BEGIN
+    UPDATE storage
+      SET totalEntriesCount=totalEntriesCount-1,
+          totalEntriesSize =totalEntriesSize -OLD.size
+      WHERE storage.id=OLD.storageId;
+    UPDATE storage
+      SET totalEntriesCount=totalEntriesCount+1,
+          totalEntriesSize =totalEntriesSize +NEW.size
+      WHERE storage.id=NEW.storageId;
+  END;
+CREATE TRIGGER filesUpdateName AFTER UPDATE OF name ON files
   BEGIN
     DELETE FROM FTS_files WHERE fileId = OLD.id;
     INSERT INTO FTS_files VALUES (NEW.id,NEW.name);
@@ -120,6 +179,7 @@ CREATE TABLE IF NOT EXISTS images(
   FOREIGN KEY(storageId) REFERENCES storage(id)
 );
 CREATE INDEX IF NOT EXISTS imagesIndex ON images (storageId,name);
+
 CREATE VIRTUAL TABLE FTS_images USING FTS4(
   imageId,
   name,
@@ -127,15 +187,35 @@ CREATE VIRTUAL TABLE FTS_images USING FTS4(
   notindexed=imageId,
   tokenize=unicode61
 );
-CREATE TRIGGER imageInsert AFTER INSERT ON images
+
+CREATE TRIGGER imagesInsert AFTER INSERT ON images
   BEGIN
+    UPDATE storage
+      SET totalEntriesCount=totalEntriesCount+1,
+          totalEntriesSize =totalEntriesSize +NEW.size
+      WHERE storage.id=NEW.storageId;
     INSERT INTO FTS_images VALUES (NEW.id,NEW.name);
   END;
-CREATE TRIGGER imageDelete BEFORE DELETE ON images
+CREATE TRIGGER imagesDelete BEFORE DELETE ON images
   BEGIN
+    UPDATE storage
+      SET totalEntriesCount=totalEntriesCount-1,
+          totalEntriesSize =totalEntriesSize -OLD.size
+      WHERE storage.id=OLD.storageId;
     DELETE FROM FTS_images WHERE imageId = OLD.id;
   END;
-CREATE TRIGGER imageUpdate AFTER UPDATE OF name ON images
+CREATE TRIGGER imagesUpdateSize AFTER UPDATE OF storageId,size ON images
+  BEGIN
+    UPDATE storage
+      SET totalEntriesCount=totalEntriesCount-1,
+          totalEntriesSize =totalEntriesSize -OLD.size
+      WHERE storage.id=OLD.storageId;
+    UPDATE storage
+      SET totalEntriesCount=totalEntriesCount+1,
+          totalEntriesSize =totalEntriesSize +NEW.size
+      WHERE storage.id=NEW.storageId;
+  END;
+CREATE TRIGGER imagesUpdateName AFTER UPDATE OF name ON images
   BEGIN
     DELETE FROM FTS_images WHERE imageId = OLD.id;
     INSERT INTO FTS_images VALUES (NEW.id,NEW.name);
@@ -156,6 +236,7 @@ CREATE TABLE IF NOT EXISTS directories(
   FOREIGN KEY(storageId) REFERENCES storage(id)
 );
 CREATE INDEX IF NOT EXISTS directoriesIndex ON directories (storageId,name);
+
 CREATE VIRTUAL TABLE FTS_directories USING FTS4(
   directoryId,
   name,
@@ -163,15 +244,31 @@ CREATE VIRTUAL TABLE FTS_directories USING FTS4(
   notindexed=directoryId,
   tokenize=unicode61
 );
-CREATE TRIGGER directoryInsert AFTER INSERT ON directories
+
+CREATE TRIGGER directoriesInsert AFTER INSERT ON directories
   BEGIN
+    UPDATE storage
+      SET totalEntriesCount=totalEntriesCount+1
+      WHERE storage.id=NEW.storageId;
     INSERT INTO FTS_directories VALUES (NEW.id,NEW.name);
   END;
-CREATE TRIGGER directoryDelete BEFORE DELETE ON directories
+CREATE TRIGGER directoriesDelete BEFORE DELETE ON directories
   BEGIN
+    UPDATE storage
+      SET totalEntriesCount=totalEntriesCount-1
+      WHERE storage.id=OLD.storageId;
     DELETE FROM FTS_directories WHERE directoryId = OLD.id;
   END;
-CREATE TRIGGER directoryUpdate AFTER UPDATE OF name ON directories
+CREATE TRIGGER directoriesUpdateSize AFTER UPDATE ON directories
+  BEGIN
+    UPDATE storage
+      SET totalEntriesCount=totalEntriesCount-1
+      WHERE storage.id=OLD.storageId;
+    UPDATE storage
+      SET totalEntriesCount=totalEntriesCount+1
+      WHERE storage.id=NEW.storageId;
+  END;
+CREATE TRIGGER directoriesUpdateName AFTER UPDATE OF name ON directories
   BEGIN
     DELETE FROM FTS_directories WHERE directoryId = OLD.id;
     INSERT INTO FTS_directories VALUES (NEW.id,NEW.name);
@@ -193,6 +290,7 @@ CREATE TABLE IF NOT EXISTS links(
   FOREIGN KEY(storageId) REFERENCES storage(id)
 );
 CREATE INDEX IF NOT EXISTS linksIndex ON links (storageId,name);
+
 CREATE VIRTUAL TABLE FTS_links USING FTS4(
   linkId,
   name,
@@ -200,15 +298,31 @@ CREATE VIRTUAL TABLE FTS_links USING FTS4(
   notindexed=linkId,
   tokenize=unicode61
 );
-CREATE TRIGGER linkInsert AFTER INSERT ON links
+
+CREATE TRIGGER linksInsert AFTER INSERT ON links
   BEGIN
+    UPDATE storage
+      SET totalEntriesCount=totalEntriesCount+1
+      WHERE storage.id=NEW.storageId;
     INSERT INTO FTS_links VALUES (NEW.id,NEW.name);
   END;
-CREATE TRIGGER linkDelete BEFORE DELETE ON links
+CREATE TRIGGER linksDelete BEFORE DELETE ON links
   BEGIN
+    UPDATE storage
+      SET totalEntriesCount=totalEntriesCount-1
+      WHERE storage.id=OLD.storageId;
     DELETE FROM FTS_links WHERE linkId = OLD.id;
   END;
-CREATE TRIGGER linkUpdate AFTER UPDATE OF name ON links
+CREATE TRIGGER linsUpdateSize AFTER UPDATE ON links
+  BEGIN
+    UPDATE storage
+      SET totalEntriesCount=totalEntriesCount-1
+      WHERE storage.id=OLD.storageId;
+    UPDATE storage
+      SET totalEntriesCount=totalEntriesCount+1
+      WHERE storage.id=NEW.storageId;
+  END;
+CREATE TRIGGER linksUpdateName AFTER UPDATE OF name ON links
   BEGIN
     DELETE FROM FTS_links WHERE linkId = OLD.id;
     INSERT INTO FTS_links VALUES (NEW.id,NEW.name);
@@ -232,6 +346,7 @@ CREATE TABLE IF NOT EXISTS hardlinks(
   FOREIGN KEY(storageId) REFERENCES storage(id)
 );
 CREATE INDEX IF NOT EXISTS hardlinksIndex ON hardlinks (storageId,name);
+
 CREATE VIRTUAL TABLE FTS_hardlinks USING FTS4(
   hardlinkId,
   name,
@@ -239,15 +354,35 @@ CREATE VIRTUAL TABLE FTS_hardlinks USING FTS4(
   notindexed=hardlinkId,
   tokenize=unicode61
 );
-CREATE TRIGGER hardlinkInsert AFTER INSERT ON hardlinks
+
+CREATE TRIGGER hardlinksInsert AFTER INSERT ON hardlinks
   BEGIN
+    UPDATE storage
+      SET totalEntriesCount=totalEntriesCount+1,
+          totalEntriesSize =totalEntriesSize +NEW.size
+      WHERE storage.id=NEW.storageId;
     INSERT INTO FTS_hardlinks VALUES (NEW.id,NEW.name);
   END;
-CREATE TRIGGER hardlinkDelete BEFORE DELETE ON hardlinks
+CREATE TRIGGER hardlinksDelete BEFORE DELETE ON hardlinks
   BEGIN
+    UPDATE storage
+      SET totalEntriesCount=totalEntriesCount-1,
+          totalEntriesSize =totalEntriesSize -OLD.size
+      WHERE storage.id=OLD.storageId;
     DELETE FROM FTS_hardlinks WHERE hardlinkId = OLD.id;
   END;
-CREATE TRIGGER hardlinkUpdate AFTER UPDATE OF name ON hardlinks
+CREATE TRIGGER hardlinksUpdateSize AFTER UPDATE OF storageId,size ON hardlinks
+  BEGIN
+    UPDATE storage
+      SET totalEntriesCount=totalEntriesCount-1,
+          totalEntriesSize =totalEntriesSize -OLD.size
+      WHERE storage.id=OLD.storageId;
+    UPDATE storage
+      SET totalEntriesCount=totalEntriesCount+1,
+          totalEntriesSize =totalEntriesSize +NEW.size
+      WHERE storage.id=NEW.storageId;
+  END;
+CREATE TRIGGER hardlinksUpdateName AFTER UPDATE OF name ON hardlinks
   BEGIN
     DELETE FROM FTS_hardlinks WHERE hardlinkId = OLD.id;
     INSERT INTO FTS_hardlinks VALUES (NEW.id,NEW.name);
@@ -271,6 +406,7 @@ CREATE TABLE IF NOT EXISTS special(
   FOREIGN KEY(storageId) REFERENCES storage(id)
 );
 CREATE INDEX IF NOT EXISTS specialIndex ON special (storageId,name);
+
 CREATE VIRTUAL TABLE FTS_special USING FTS4(
   specialId,
   name,
@@ -278,15 +414,31 @@ CREATE VIRTUAL TABLE FTS_special USING FTS4(
   notindexed=specialId,
   tokenize=unicode61
 );
+
 CREATE TRIGGER specialInsert AFTER INSERT ON special
   BEGIN
+    UPDATE storage
+      SET totalEntriesCount=totalEntriesCount+1
+      WHERE storage.id=NEW.storageId;
     INSERT INTO FTS_special VALUES (NEW.id,NEW.name);
   END;
 CREATE TRIGGER specialDelete BEFORE DELETE ON special
   BEGIN
+    UPDATE storage
+      SET totalEntriesCount=totalEntriesCount-1
+      WHERE storage.id=OLD.storageId;
     DELETE FROM FTS_special WHERE specialId = OLD.id;
   END;
-CREATE TRIGGER specialUpdate AFTER UPDATE OF name ON special
+CREATE TRIGGER specialUpdateSize AFTER UPDATE ON special
+  BEGIN
+    UPDATE storage
+      SET totalEntriesCount=totalEntriesCount-1
+      WHERE storage.id=OLD.storageId;
+    UPDATE storage
+      SET totalEntriesCount=totalEntriesCount+1
+      WHERE storage.id=NEW.storageId;
+  END;
+CREATE TRIGGER specialUpdateName AFTER UPDATE OF name ON special
   BEGIN
     DELETE FROM FTS_special WHERE specialId = OLD.id;
     INSERT INTO FTS_special VALUES (NEW.id,NEW.name);
