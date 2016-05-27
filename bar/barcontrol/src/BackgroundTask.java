157d167730cd0c862f719d525093b58b86f38dea
Torsten Rupp <torsten.rupp@gmx.net>
2016-05-27 06:49:05 +0000

    background task

---
+++ bar/barcontrol/src/BackgroundTask.java
@@ -0,0 +1,68 @@
+/***********************************************************************\
+*
+* $Source: /home/torsten/cvs/bar/barcontrol/src/TabRestore.java,v $
+* $Revision: 1.17 $
+* $Author: torsten $
+* Contents: background task
+* Systems: all
+*
+\***********************************************************************/
+
+/****************************** Imports ********************************/
+
+// base
+
+/****************************** Classes ********************************/
+
+/** background task
+ */
+public abstract class BackgroundTask
+{
+  // --------------------------- constants --------------------------------
+
+  // --------------------------- variables --------------------------------
+  private final BusyDialog busyDialog;
+  private Thread           thread;
+
+  // ------------------------ native functions ----------------------------
+
+  // ---------------------------- methods ---------------------------------
+
+  /** create background task
+   * @param busyDialog busy dialog
+   * @param userData user data
+   */
+  BackgroundTask(final BusyDialog busyDialog, final Object userData)
+  {
+    final BackgroundTask backgroundTask = this;
+
+    this.busyDialog = busyDialog;
+
+    thread = new Thread(new Runnable()
+    {
+      @Override
+      public void run()
+      {
+        backgroundTask.run(busyDialog,userData);
+      }
+    });
+    thread.setDaemon(true);
+    thread.start();
+  }
+
+  /** create background task
+   * @param busyDialog busy dialog
+   */
+  BackgroundTask(final BusyDialog busyDialog)
+  {
+    this(busyDialog,null);
+  }
+
+  /** run method
+   * @param busyDialog busy dialog
+   * @param userData user data
+   */
+  abstract public void run(BusyDialog busyDialog, Object userData);
+}
+
+/* end of file */
