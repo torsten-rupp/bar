fc0e5e8c7c42d7a9c50094ce3ce0b9869187000a
Torsten Rupp <torsten.rupp@gmx.net>
2017-02-13 10:13:16 +0000

    - removed priority - clean-up

--- bar/bar/continuous.c
+++ bar/bar/continuous.c
@@ -230,9 +230,9 @@ LOCAL void printNotifies(void)
 
   // open continuous database
   #ifdef NDEBUG
-    error = Database_open(databaseHandle,databaseFileName,DATABASE_OPENMODE_READWRITE,DATABASE_PRIORITY_HIGH,NO_WAIT);
+    error = Database_open(databaseHandle,databaseFileName,DATABASE_OPENMODE_READWRITE,NO_WAIT);
   #else /* not NDEBUG */
-    error = __Database_open(__fileName__,__lineNb__,databaseHandle,databaseFileName,DATABASE_OPENMODE_READWRITE,DATABASE_PRIORITY_HIGH,NO_WAIT);
+    error = __Database_open(__fileName__,__lineNb__,databaseHandle,databaseFileName,DATABASE_OPENMODE_READWRITE,NO_WAIT);
   #endif /* NDEBUG */
   if (error != ERROR_NONE)
   {
@@ -273,9 +273,9 @@ LOCAL void printNotifies(void)
   // create continuous database
   if (databaseFileName != NULL) (void)File_deleteCString(databaseFileName,FALSE);
   #ifdef NDEBUG
-    error = Database_open(databaseHandle,databaseFileName,DATABASE_OPENMODE_CREATE,DATABASE_PRIORITY_HIGH,NO_WAIT);
+    error = Database_open(databaseHandle,databaseFileName,DATABASE_OPENMODE_CREATE,NO_WAIT);
   #else /* not NDEBUG */
-    error = __Database_open(__fileName__,__lineNb__,databaseHandle,databaseFileName,DATABASE_OPENMODE_CREATE,DATABASE_PRIORITY_HIGH,NO_WAIT);
+    error = __Database_open(__fileName__,__lineNb__,databaseHandle,databaseFileName,DATABASE_OPENMODE_CREATE,NO_WAIT);
   #endif /* NDEBUG */
   if (error != ERROR_NONE)
   {
