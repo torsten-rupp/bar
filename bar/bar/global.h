aaae35cf83fb31b9f68d70768b69b05c6c47a6fc
Torsten Rupp <torsten.rupp@gmx.net>
2018-05-01 07:06:03 +0000

    fixed intX datatypes

--- bar/bar/global.h
+++ bar/bar/global.h
@@ -239,18 +239,18 @@ typedef enum
 // base datatypes
 typedef uint8_t             byte;
 
-typedef unsigned char       bool8;
-typedef unsigned int        bool32;
+typedef uint8_t             bool8;
+typedef uint32_t            bool32;
 typedef char                char8;
 typedef unsigned char       uchar8;
-typedef char                int8;
-typedef short int           int16;
-typedef int                 int32;
-typedef long long int       int64;
-typedef unsigned char       uint8;
-typedef unsigned short int  uint16;
-typedef unsigned int        uint32;
-typedef unsigned long long  uint64;
+typedef int8_t              int8;
+typedef int16_t             int16;
+typedef int32_t             int32;
+typedef int64_t             int64;
+typedef uint8_t             uint8;
+typedef uint16_t            uint16;
+typedef uint32_t            uint32;
+typedef uint64_t            uint64;
 typedef void                void32;
 
 // mask+shift data
