782516bc8bac1c1a8401ab7704532b27da1de60e
Torsten Rupp <torsten.rupp@gmx.net>
2018-09-08 19:18:42 +0000

    fixed format age

--- bar/barcontrol/src/BARControl.java
+++ bar/barcontrol/src/BARControl.java
@@ -1248,14 +1248,14 @@ class Age
    */
   public static String format(int age)
   {
-    if      (age == 365      ) return               BARControl.tr("1 year"  );
-    else if ((age % 365) == 0) return String.format(BARControl.tr("%d years"),age/365);
-    else if (age == 30       ) return               BARControl.tr("1 month" );
-    else if ((age % 30) == 0 ) return String.format(BARControl.tr("%d month"),age/30);
-    else if (age == 7        ) return               BARControl.tr("1 week"  );
-    else if ((age % 7) == 0  ) return String.format(BARControl.tr("%d weeks"),age/7);
-    else if (age == 1        ) return               BARControl.tr("1 day"   );
-    else                       return String.format(BARControl.tr("%d days" ),age);
+    if      (age == 0        ) return BARControl.tr("{0} {0,choice,0#days|1#day|1<days}",      0      );
+    else if (age == 365      ) return BARControl.tr("{0} {0,choice,0#years|1#year|1<years}",   1      );
+    else if ((age % 365) == 0) return BARControl.tr("{0} {0,choice,0#years|1#year|1<years}",   age/365);
+    else if (age == 30       ) return BARControl.tr("{0} {0,choice,0#months|1#month|1<months}",1      );
+    else if ((age % 30) == 0 ) return BARControl.tr("{0} {0,choice,0#months|1#month|1<months}",age/30 );
+    else if (age == 7        ) return BARControl.tr("{0} {0,choice,0#weeks|1#week|1<weeks}",   1      );
+    else if ((age % 7) == 0  ) return BARControl.tr("{0} {0,choice,0#weeks|1#week|1<weeks}",   age/7  );
+    else                       return BARControl.tr("{0} {0,choice,0#days|1#day|1<days}",      age    );
   }
 }
 
