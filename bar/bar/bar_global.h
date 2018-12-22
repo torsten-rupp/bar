c649cc6466a04bdafff44cf6c8496954f537345f
Torsten Rupp <torsten.rupp@gmx.net>
2018-12-22 10:52:20 +0000

    master: added buffer

--- bar/bar/bar_global.h
+++ bar/bar/bar_global.h
@@ -547,26 +547,47 @@ typedef struct
 // status info data
 typedef struct
 {
-  ulong     doneCount;                     // number of entries processed
-  uint64    doneSize;                      // number of bytes processed
-  ulong     totalEntryCount;               // total number of entries
-  uint64    totalEntrySize;                // total size of entries [bytes]
-  bool      collectTotalSumDone;           // TRUEinitOp iff all file sums are collected
-  ulong     skippedEntryCount;             // number of skipped entries
-  uint64    skippedEntrySize;              // sum of skipped bytes
-  ulong     errorEntryCount;               // number of entries with errors
-  uint64    errorEntrySize;                // sum of bytes of entries with errors
-  uint64    archiveSize;                   // number of bytes stored in archive
-  double    compressionRatio;              // compression ratio
-  String    entryName;                     // current entry name
-  uint64    entryDoneSize;                 // number of bytes processed of current entry
-  uint64    entryTotalSize;                // total number of bytes of current entry
-  String    storageName;                   // current storage name
-  uint64    storageDoneSize;               // number of bytes processed of current archive
-  uint64    storageTotalSize;              // total bytes of current archive
-  uint      volumeNumber;                  // current volume number
-  double    volumeProgress;                // current volume progress [0..100]
-  String    message;
+  struct
+  {
+    ulong     count;                                          // number of entries processed
+    uint64    size;                                           // number of bytes processed
+  } done;
+  struct
+  {
+    ulong     count;                                          // total number of entries
+    uint64    size;                                           // total size of entries [bytes]
+  } totalEntries;
+  bool      collectTotalSumDone;                              // TRUEinitOp iff all file sums are collected
+  struct
+  {
+    ulong     count;                                          // number of skipped entries
+    uint64    size;                                           // sum of skipped bytes
+  } skippedEntries;
+  struct
+  {
+    ulong     count;                                          // number of entries with errors
+    uint64    size;                                           // sum of bytes of entries with errors
+  } errorEntries;
+  uint64 archiveSize;                                         // number of bytes stored in archive
+  double compressionRatio;                                    // compression ratio
+  struct
+  {
+    String    name;                                           // current entry name
+    uint64    doneSize;                                       // number of bytes processed of current entry
+    uint64    totalSize;                                      // total number of bytes of current entry
+  } entry;
+  struct
+  {
+    String    name;                                           // current storage name
+    uint64    doneSize;                                       // number of bytes processed of current archive
+    uint64    totalSize;                                      // total bytes of current archive
+  } storage;
+  struct
+  {
+    uint      number;                                         // current volume number
+    double    progress;                                       // current volume progress [0..100]
+  } volume;
+  String message;
 } StatusInfo;
 
 /***********************************************************************\
