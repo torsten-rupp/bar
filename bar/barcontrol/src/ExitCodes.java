a74f71ddcce9cd8d3306eb686e92b4d49304f232
Torsten Rupp <torsten.rupp@gmx.net>
2019-10-21 12:55:52 +0000

    : exit codes

---
+++ bar/barcontrol/src/ExitCodes.java
@@ -0,0 +1,25 @@
+/***********************************************************************\
+*
+* $Revision: 1.30 $
+* $Date: 2019-01-17 21:06:25 +0100 (Thu, 17 Jan 2019) $
+* $Author: torsten $
+* Contents: exit code
+* Systems: all
+*
+\***********************************************************************/
+
+/****************************** Imports ********************************/
+
+/****************************** Classes ********************************/
+
+/** exit codes
+ */
+public class ExitCodes
+{
+  final static int OK             =   0;
+  final static int FAIL           =   1;
+  final static int RESTART        =  64;
+  final static int INTERNAL_ERROR = 127;
+}
+
+/* end of file */
