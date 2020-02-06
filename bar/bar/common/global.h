10338d96d98b6895497d113c54944bf7592d06d7
Torsten Rupp <torsten.rupp@gmx.net>
2020-02-06 20:21:52 +0000

    : Win32: fixed warning

--- bar/bar/common/global.h
+++ bar/bar/common/global.h
@@ -49,6 +49,7 @@
 #if   defined(PLATFORM_LINUX)
   #include <pthread.h>
 #elif defined(PLATFORM_WINDOWS)
+  #include <winsock2.h>
   #include <windows.h>
   #include <pthread.h>
   #include <intrin.h>
