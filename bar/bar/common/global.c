5173388de0adf4e48decaff71f00b1c7177851cc
Torsten Rupp <torsten.rupp@gmx.net>
2020-12-25 23:07:36 +0000

    fixed includes

--- bar/bar/common/global.c
+++ bar/bar/common/global.c
@@ -17,9 +17,13 @@
 #include <inttypes.h>
 #include <unistd.h>
 #include <pthread.h>
-#ifdef HAVE_REGEX_H
+#if defined(HAVE_PCRE)
+  #include <pcreposix.h>
+#elif defined(HAVE_REGEX_H)
   #include <regex.h>
-#endif
+#else
+  #warning No regular expression library available!
+#endif /* HAVE_PCRE || HAVE_REGEX_H */
 #ifdef HAVE_GCRYPT
   #pragma GCC diagnostic ignored "-Wdeprecated-declarations"
   #include <gcrypt.h>
