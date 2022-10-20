cd3856dc51554072b322474dde3d98081af82a7e
Torsten Rupp <torsten.rupp@macio.de>
2022-10-21 01:17:28 +0200

    fixed TLS disconnect

--- bar/archive.c
+++ bar/archive.c
@@ -15721,25 +15721,9 @@ Errors Archive_updateIndex(IndexHandle       *indexHandle,
             break;
           }
 
-          if (!INDEX_ID_IS_NONE(entityId))
+          // create entity if not exists with given job UUID/entity UUID/host name/archive type
+          if (INDEX_ID_IS_NONE(entityId))
           {
-            // update entity
-// TODO: do not update entity!
-#if 0
-            error = Index_updateEntity(indexHandle,
-                                       entityId,
-                                       String_cString(jobUUID),
-                                       String_cString(entityUUID),
-                                       String_cString(hostName),
-                                       String_cString(userName),
-                                       archiveType,
-                                       createdDateTime
-                                      );
-#endif
-          }
-          else
-          {
-            // check if entity with given job UUID/entity UUID/host name/archive type exists, otherwise create that entity
             error = Index_findEntity(indexHandle,
                                      INDEX_ID_NONE,  // findEntityIndexId
                                      jobUUID,
@@ -15773,11 +15757,11 @@ Errors Archive_updateIndex(IndexHandle       *indexHandle,
                                       &entityId
                                      );
             }
-          }
-          if (error != ERROR_NONE)
-          {
-            (void)Archive_closeEntry(&archiveEntryInfo);
-            break;
+            if (error != ERROR_NONE)
+            {
+              (void)Archive_closeEntry(&archiveEntryInfo);
+              break;
+            }
           }
 
           // flush index list
