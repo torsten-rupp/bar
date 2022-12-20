303da290c2dea39d528a41a69e0706acd72fc618
Torsten Rupp <torsten.rupp@macio.de>
2022-12-20 13:26:00 +0100

    fixed configuration issue handling: report as error

--- bar/bar.c
+++ bar/bar.c
@@ -4401,7 +4401,7 @@ LOCAL Errors bar(int argc, const char *argv[])
   error = Configuration_readAll(isPrintInfo(2) || printInfoFlag);
   if (error != ERROR_NONE)
   {
-    printWarning("%s",Error_getText(error));
+    return error;
   }
 
   // special case: set verbose level/quiet flag in interactive mode
