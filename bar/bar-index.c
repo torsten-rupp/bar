499c590fb6860100264799513dcb57ac8ab091de
Torsten Rupp <torsten.rupp@gmx.net>
2023-07-14 05:17:26 +0200

    clean duplicate: fixed error handling

--- bar/bar-index.c
+++ bar/bar-index.c
@@ -7453,6 +7453,9 @@ LOCAL Errors cleanDuplicateEntries(DatabaseHandle *databaseHandle)
   {
     printInfo("FAIL!\n");
     printError("clean duplicates fail (error: %s)!",Error_getText(error));
+    Array_done(&ids);
+    String_delete(name);
+    return error;
   }
   n = 0L;
   DATABASE_TRANSACTION_DO(databaseHandle,DATABASE_TRANSACTION_TYPE_EXCLUSIVE,WAIT_FOREVER)
@@ -7478,6 +7481,14 @@ LOCAL Errors cleanDuplicateEntries(DatabaseHandle *databaseHandle)
         n++;
       }
     }
+  }
+  if (error != ERROR_NONE)
+  {
+    printInfo("FAIL!\n");
+    printError("clean duplicates fail (error: %s)!",Error_getText(error));
+    Array_done(&ids);
+    String_delete(name);
+    return error;
   }
   printInfo("%lu\n",n);
   totalCount += n;
