ccf10c017156e14a1f038ae9f79ed08aab58177b
Torsten Rupp <torsten.rupp@gmx.net>
2026-01-31 16:20:29 +0100

    fix error handling open directory list

--- bar/commands_list.c
+++ bar/commands_list.c
@@ -3659,13 +3659,13 @@ Errors Command_list(StringList              *storageNameList,
     if (error != ERROR_NONE)
     {
       StorageDirectoryListHandle storageDirectoryListHandle;
-      if (Storage_openDirectoryList(&storageDirectoryListHandle,
-                                    &storageSpecifier,
-                                    NULL,  // fileName
-                                    jobOptions,
-                                    SERVER_CONNECTION_PRIORITY_HIGH
-                                   ) == ERROR_NONE
-         )
+      error = Storage_openDirectoryList(&storageDirectoryListHandle,
+                                        &storageSpecifier,
+                                        NULL,  // fileName
+                                        jobOptions,
+                                        SERVER_CONNECTION_PRIORITY_HIGH
+                                       );
+      if (error == ERROR_NONE)
       {
         String fileName = String_new();
         while (!Storage_endOfDirectoryList(&storageDirectoryListHandle))
@@ -3717,13 +3717,13 @@ Errors Command_list(StringList              *storageNameList,
     {
       // open directory list
       StorageDirectoryListHandle storageDirectoryListHandle;
-      if (Storage_openDirectoryList(&storageDirectoryListHandle,
-                                    &storageSpecifier,
-                                    NULL,  // fileName
-                                    jobOptions,
-                                    SERVER_CONNECTION_PRIORITY_HIGH
-                                   ) == ERROR_NONE
-         )
+      error = Storage_openDirectoryList(&storageDirectoryListHandle,
+                                        &storageSpecifier,
+                                        NULL,  // fileName
+                                        jobOptions,
+                                        SERVER_CONNECTION_PRIORITY_HIGH
+                                       );
+      if (error == ERROR_NONE)
       {
         if (String_isEmpty(storageSpecifier.archivePatternString))
         {
